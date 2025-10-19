9064b2812eee7d6a613cd35437993bd6434495ec
Jenea-G <eugeniagurulea@gmail.com>
2025-10-19 12:28:54 -0400

    ajout de js pour creation de salutation au survol de Loti dans le banniere

---
+++ index.js
@@ -0,0 +1,25 @@
+// Message de salutation au survol de Loti dans le banniere de l'accueil
+let monImage = document.getElementById("mon-image");
+
+monImage.addEventListener("mouseover", () => afficherSalutation());
+monImage.addEventListener("mouseout", () => cacherSalutation());
+
+function afficherSalutation() {
+    //Création du nouveau paragraphe orphelin
+    let titreOrphelin = document.createElement("h3");
+    titreOrphelin.textContent = "Bonjour mon ami !";
+    titreOrphelin.className = "";
+    //Sélection du div parent adoptif
+    let parentAdoptif = document.getElementById("div-adoptif");
+    //Le div parent adopte un nouvel enfant :)
+    parentAdoptif.appendChild(titreOrphelin);
+}
+
+function cacherSalutation() {
+    let parentAdoptif = document.getElementById("div-adoptif");
+    let titreOrphelin = parentAdoptif.querySelector("h3");
+    //Le div parent supprime l'enfant :(
+    parentAdoptif.removeChild(titreOrphelin);
+}
+
+
